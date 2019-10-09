425deb0ebed0de1c355399b685db5ee6f25b0a63
LucienChotard <chotardlucien@gmail.com>
2019-10-09 20:01:11 +0200

    parallax no jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-$.jInvertScroll(['.scroll']);
+const parallax = document.querySelectorAll('.parallax')
+const sun=document.querySelector("#sunbox")
+let parallaxfactor=0
+
+
+function Scroll(){
+    let scrollPos = window.pageXOffset
+    for(let i=0; i<parallax.length; i++){
+      parallax[i].style.transform="translateX("+scrollPos/parallax[i].dataset.speed+"px)"
+      parallaxfactor+=1
+    }
+    sun.style.transform = "rotate("+scrollPos/26+"deg)";
+}
+window.addEventListener("scroll",Scroll);

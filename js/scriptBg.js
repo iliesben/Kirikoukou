b714be911be7ec5c70e53b64496690f3a4983435
IlièsBenos <iliesbenosman@gmail.com>
2019-10-11 00:56:51 +0200

    correction of bg

--- js/scriptBg.js
+++ js/scriptBg.js
@@ -1,50 +1,37 @@
-// first page
+// index page
 
-let container = document.getElementById('container'),
-    image = document.getElementById('image'),
-    image2 = document.getElementById('image2'),
-    main = document.querySelector('main'),
-    body = document.querySelector('body')
+let container = document.getElementById("container"),
+    imageBackGround = document.getElementById("imageBackGround"),
+    imageTextKirikou = document.getElementById("imageTextKirikou"),
+    main = document.querySelector("main"),
+    body = document.querySelector("body")
 
 let windowWidth = window.innerWidth,
     windowHeight = window.innerHeight;
 
 
 function moveBackgound(e) {
-  //getting mopuse x and y position
 
-  let mouseXposition = e.clientX,
-      mouseYposition = e.clientY;
-  
-// we dont have to use window's width and height to calculate this, u can just replace it with any number you want , prefered to be more than 900 based on pic's width and height
-  
-  // the more u increase the number devided by width and height the less friction u have ,
-  // and the less u decrease the devided by width and height  the more friction u have, but be carefull bcs the pic may go out of the body
-  let calculatedX = mouseXposition / (windowWidth / 5);
-  let calculatedY = mouseYposition / (windowHeight / 5);
+    let mouseXposition = e.clientX,
+        mouseYposition = e.clientY;
 
-  //OR USE THIS
-  
-  // the more u increase the number the less friction u have ,
-  // and the less u decrease the number the more friction u have, but be carefull bcs the pic may go out of the body
-  // let calculatedX = mouseXposition / 200;
-  // let calculatedY = mouseYposition / 200;
+    let calculatedX = mouseXposition / (windowWidth / 5);
+    let calculatedY = mouseYposition / (windowHeight / 5);
 
-  image.style.transform = "translate(-" + calculatedX.toString() + "%, -" + calculatedY.toString() + "%)";  
-  image2.style.transform = "translate(" + calculatedX.toString() + "%, " + calculatedY.toString() + "%)"; 
+    imageBackGround.style.transform = "translate(-" + calculatedX.toString() + "%, -" + calculatedY.toString() + "%)";
+    imageTextKirikou.style.transform = "translate(" + calculatedX.toString() + "%, " + calculatedY.toString() + "%)";
 }
 
 container.addEventListener("mousemove", moveBackgound)
 
 
-container.addEventListener("click", function(){
+container.addEventListener("click", function() {
 
-        myVar = setTimeout(function(){ 
+    myVar = setTimeout(function() {
             window.location = "../Kirikoukou/pages/village.html"
-         }, 
-            2000);
-    
-        container.classList.add('agrand')
+        },
+        2000);
 
-    
+    container.classList.add("backGroundAnnimation")
+
 });
--- js/scriptKaraba.js
+++ js/scriptKaraba.js
@@ -1,63 +1,77 @@
+// Kirikou annimation
+
 const sun=document.querySelector("#sunbox")
 
 let tab = ["../img/kirikouStop.png","KirikouImgStop rellax"]
 
-let imgKirikouRun = document.querySelector('.KirikouImgRun')
+let imgKirikouRun = document.querySelector(".KirikouImgRun")
 
 const divKirikou = document.querySelector(".kirikou")
-let imgKirikouStop = document.createElement('img')
+let imgKirikouStop = document.createElement("img")
 divKirikou.appendChild(imgKirikouStop)
 
-imgKirikouRun.style.display='none'
-imgKirikouStop.setAttribute('src',tab[0])
-imgKirikouStop.setAttribute('class',tab[1])
+imgKirikouRun.style.display="none"
+imgKirikouStop.setAttribute("src",tab[0])
+imgKirikouStop.setAttribute("class",tab[1])
 
 
 function Scroll(){
-    let scrollPos = window.pageXOffset
+  let scrollPos = window.pageXOffset
 
-    if(scrollPos > 0 && scrollPos < 19){
-        imgKirikouRun.style.display='none'
-        imgKirikouStop.setAttribute('src',tab[0])
-        imgKirikouStop.setAttribute('class',tab[1])
-        console.log('1')
-    }
-    if(scrollPos > 20 && scrollPos < 3999){
-        imgKirikouRun.style.display='block'
-        imgKirikouStop.removeAttribute('src',tab[0])
-        imgKirikouStop.removeAttribute('class',tab[1])
-        console.log('2')
-    }
-    if(scrollPos > 4000){
-        console.log('3')
-        window.location = "../pages/boutique.html"
-    }
-    sun.style.transform = "rotate("+scrollPos/26+"deg)";
-    console.log(scrollPos)
-
+  if(scrollPos > 0 && scrollPos < 19){
+    imgKirikouRun.style.display="none"
+    imgKirikouStop.setAttribute("src",tab[0])
+    imgKirikouStop.setAttribute("class",tab[1])
+  }
+  if(scrollPos > 20 && scrollPos < 3999){
+    imgKirikouRun.style.display="block"
+    imgKirikouStop.removeAttribute("src",tab[0])
+    imgKirikouStop.removeAttribute("class",tab[1])
+  }
+  if(scrollPos > 4000){
+    window.location = "../pages/boutique.html"
+  }
+  sun.style.transform = "rotate("+scrollPos/26+"deg)";
 }
 window.addEventListener("scroll",Scroll);
 
+//Menu work
 
-
-let menuWatcher = document.querySelector('.menuImg')
-let MenuNav = document.querySelector('.menuUl')
+let menuWatcher = document.querySelector(".menuImg")
+let MenuNav = document.querySelector(".menuUl")
 let i = 0
 
 function Menu(){
-    if(i==0)
-    {
-      console.log('0')
-      MenuNav.style.transform='translateX(0px)'
-      i++
-    }
-    else if(i==1)
-    {
-      console.log('1')
-      MenuNav.style.transform='translateX(200px)'
-      i--
-    }
+  if(i==0)
+  {
+    MenuNav.style.transform="translateX(0px)"
+    i++
+  }
+  else if(i==1)
+  {
+    MenuNav.style.transform="translateX(200px)"
+    i--
+  }
 
 }
 MenuNav.addEventListener("click", Menu)
 
+// Eyes Flow cursor
+
+let balls = document.getElementsByClassName("ball");
+
+document.addEventListener("mousemove", function(event)
+{
+  const x = event.clientX / window.innerWidth
+  const y = event.clientY / window.innerHeight
+  const angle = Math.atan2(y, 1 - x)
+
+  const distance = Math.hypot(window.innerWidth - event.clientX, event.clientY)
+  const radius = Math.min(distance / 70, 10)
+
+  const eyeX = Math.sin(angle - Math.PI * 0.5) * radius
+  const eyeY = Math.cos(angle - Math.PI * 0.5) * radius
+
+  balls[0].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
+  balls[1].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
+})

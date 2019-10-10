9991f5ba6c9526d19c4744d096506f9b00fbff6d
IlièsBenos <iliesbenosman@gmail.com>
2019-10-11 01:51:36 +0200

    logo

--- js/scriptsSource.js
+++ js/scriptsSource.js
@@ -1,8 +1,8 @@
 // Kirikou annimation
 
-const sun=document.querySelector("#sunbox")
+const sun = document.querySelector("#sunbox")
 
-let tab = ["../img/kirikouStop.png","KirikouImgStop rellax"]
+let tab = ["../img/kirikouStop.png", "KirikouImgStop rellax"]
 
 let imgKirikouRun = document.querySelector(".KirikouImgRun")
 
@@ -10,30 +10,30 @@ const divKirikou = document.querySelector(".kirikou")
 let imgKirikouStop = document.createElement("img")
 divKirikou.appendChild(imgKirikouStop)
 
-imgKirikouRun.style.display="none"
-imgKirikouStop.setAttribute("src",tab[0])
-imgKirikouStop.setAttribute("class",tab[1])
+imgKirikouRun.style.display = "none"
+imgKirikouStop.setAttribute("src", tab[0])
+imgKirikouStop.setAttribute("class", tab[1])
 
 
-function Scroll(){
-  let scrollPos = window.pageXOffset
+function Scroll() {
+    let scrollPos = window.pageXOffset
 
-  if(scrollPos > 0 && scrollPos < 19){
-    imgKirikouRun.style.display="none"
-    imgKirikouStop.setAttribute("src",tab[0])
-    imgKirikouStop.setAttribute("class",tab[1])
-  }
-  if(scrollPos > 20 && scrollPos < 3999){
-    imgKirikouRun.style.display="block"
-    imgKirikouStop.removeAttribute("src",tab[0])
-    imgKirikouStop.removeAttribute("class",tab[1])
-  }
-  if(scrollPos > 4000){
-    window.location = "../pages/karaba.html"
-  }
-  sun.style.transform = "rotate("+scrollPos/26+"deg)";
+    if (scrollPos > 0 && scrollPos < 19) {
+        imgKirikouRun.style.display = "none"
+        imgKirikouStop.setAttribute("src", tab[0])
+        imgKirikouStop.setAttribute("class", tab[1])
+    }
+    if (scrollPos > 20 && scrollPos < 3999) {
+        imgKirikouRun.style.display = "block"
+        imgKirikouStop.removeAttribute("src", tab[0])
+        imgKirikouStop.removeAttribute("class", tab[1])
+    }
+    if (scrollPos > 4000) {
+        window.location = "../pages/karaba.html"
+    }
+    sun.style.transform = "rotate(" + scrollPos / 26 + "deg)";
 }
-window.addEventListener("scroll",Scroll);
+window.addEventListener("scroll", Scroll);
 
 //Menu work
 
@@ -42,17 +42,14 @@ let menuWatcher = document.querySelector(".menuImg")
 let menuOpen = document.querySelector(".menuUl")
 let i = 0
 
-function Menu(){
-  if(i==0)
-  {
-    menuOpen.style.transform="translateX(0px)"
-    i++
-  }
-  else if(i==1)
-  {
-    menuOpen.style.transform="translateX(200px)"
-    i--
-  }
+function Menu() {
+    if (i == 0) {
+        menuOpen.style.transform = "translateX(0px)"
+        i++
+    } else if (i == 1) {
+        menuOpen.style.transform = "translateX(200px)"
+        i--
+    }
 
 }
 menuOpen.addEventListener("click", Menu)
@@ -61,18 +58,17 @@ menuOpen.addEventListener("click", Menu)
 
 let balls = document.getElementsByClassName("ball");
 
-document.addEventListener("mousemove", function(event)
-{
-  const x = event.clientX / window.innerWidth
-  const y = event.clientY / window.innerHeight
-  const angle = Math.atan2(y, 1 - x)
+document.addEventListener("mousemove", function(event) {
+    const x = event.clientX / window.innerWidth
+    const y = event.clientY / window.innerHeight
+    const angle = Math.atan2(y, 1 - x)
 
-  const distance = Math.hypot(window.innerWidth - event.clientX, event.clientY)
-  const radius = Math.min(distance / 70, 10)
+    const distance = Math.hypot(window.innerWidth - event.clientX, event.clientY)
+    const radius = Math.min(distance / 70, 10)
 
-  const eyeX = Math.sin(angle - Math.PI * 0.5) * radius
-  const eyeY = Math.cos(angle - Math.PI * 0.5) * radius
+    const eyeX = Math.sin(angle - Math.PI * 0.5) * radius
+    const eyeY = Math.cos(angle - Math.PI * 0.5) * radius
 
-  balls[0].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
-  balls[1].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
+    balls[0].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
+    balls[1].style.transform = 'translate(' + eyeX + 'px, ' + eyeY + 'px)';
 })
